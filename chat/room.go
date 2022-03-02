a1d01991c8e62c81c849493b831d9b1a925e1908
ryuyauetake <ryuya@anymindgroup.com>
2022-03-03 00:05:13 +0900

    Chapter 1.2.2 model chatroom

--- chat/room.go
+++ chat/room.go
@@ -3,4 +3,10 @@ package main
 type room struct {
 	// forwardは他のクライアントに転送するためのメッセージを保持するチャンネルです。
 	forward chan []byte
+	// joinはチャットルームに参加しようとしているクライアントのためのチャンネルです。
+	join chan *client
+	// leaveはチャットルームから退室しようとしているクライアントのためのチャネルです。
+	leave chan *client
+	// clientsは在室しているすべてのクライアントが保持されます。
+	clients map[*client]bool
 }
