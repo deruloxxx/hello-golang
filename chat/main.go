88dd458aa5f42652239301bd854ff5a0fe6eb3ce
ryuyauetake <ryuya@anymindgroup.com>
2022-03-20 22:46:52 +0900

    Fix flag string

--- chat/main.go
+++ chat/main.go
@@ -27,7 +27,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	var addr = flag.String("host", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
