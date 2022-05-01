5ad0fd94d31a8effb2dba929711a9c9e339cb049
ryuyauetake <ryuya@anymindgroup.com>
2022-05-01 18:45:38 +0900

    feat(chat) 1.2.2

--- chat/room.go
+++ chat/room.go
@@ -4,4 +4,11 @@ type room struct {
 	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです。
 	// ここで受け取ったメッセージを全て転送する。
 	forward chan []byte
+	// 同時アクセスによる競合を防ぐためにjoin, leaveを追加。
+	// joinはチャットルームに参加しようとしているクライアントのためのチャネル
+	join chan *client
+	// leaveはチャットルームから退室しようとしているクライアントのためのチャネル
+	leave chan *client
+	// [map]clientsには在室している全てのクライアントをjoin,leaveによって保持。
+	clients map[*client]bool
 }
