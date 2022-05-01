fab24767b842eaf7fdef9f0afa6f6522b843f340
ryuyauetake <ryuya@anymindgroup.com>
2022-05-01 17:39:02 +0900

    feat(chat) chapter 1.1.2

--- chat/main.go
+++ chat/main.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"flag"
+	// text/templateよりhtml/templateの方がセキュア。
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
 	"sync"
-
-	"hello-golang/trace"
-
-	"github.com/markbates/goth"
-	"github.com/markbates/goth/providers/google"
-	"github.com/stretchr/objx"
 )
-
-// 現在アクティブなAvatarの実装
-var avatars Avatar = TryAvatars{
-	UseFileSystemAvatar,
-	UseAuthAvatar,
-	UseGravatar}
 
 // templは1つのテンプレートを表します
 type templateHandler struct {
@@ -31,55 +18,21 @@ type templateHandler struct {
 
 // ServeHTTPはHTTPリクエストを処理します
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// テンプレートファイルのパースを1度だけ実行する
 	t.once.Do(func() {
-		t.templ =
-			template.Must(template.ParseFiles(filepath.Join("templates",
-				t.filename)))
+		// mustはerrorが起きた時にnon-nilを返す。ParseFilesでテンプレートをパース。
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	data := map[string]interface{}{
-		"Host": r.Host,
-	}
-	if authCookie, err := r.Cookie("auth"); err == nil {
-		data["UserData"] = objx.MustFromBase64(authCookie.Value)
-	}
-	t.templ.Execute(w, data)
+	// nilはtsでいうany型。https://stackoverflow.com/questions/64347531/what-is-the-type-of-nil
+	t.templ.Execute(w, nil)
 }
 
 func main() {
-	var addr = flag.String("host", ":8080", "アプリケーションのアドレス")
-	flag.Parse() // フラグを解釈します
-	// Gothのセットアップ
-	goth.UseProviders(
-		google.New(os.Getenv("GOOGLE_KEY"), os.Getenv("GOOGLE_SECRET"), "http://localhost:8080/auth/google/callback"),
-	)
+	// ルート
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
-	r := newRoom()
-	r.tracer = trace.New(os.Stdout)
-	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
-	http.Handle("/login", &templateHandler{filename: "login.html"})
-	http.HandleFunc("/auth/", loginHandler)
-	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
-		http.SetCookie(w, &http.Cookie{
-			Name:   "auth",
-			Value:  "",
-			Path:   "/",
-			MaxAge: -1,
-		})
-		w.Header()["Location"] = []string{"/chat"}
-		w.WriteHeader(http.StatusTemporaryRedirect)
-	})
-	http.Handle("/upload", &templateHandler{filename: "upload.html"})
-	http.HandleFunc("/uploader", uploaderHandler)
-	http.Handle("/avatars/",
-		http.StripPrefix("/avatars/",
-			http.FileServer(http.Dir("./avatars"))))
-
-	http.Handle("/room", r)
-	// チャットルームを開始します
-	go r.run()
-	// Webサーバーを起動します
-	log.Println("Webサーバーを開始します。ポート: ", *addr)
-	if err := http.ListenAndServe(*addr, nil); err != nil {
-		log.Fatal("ListenAndServe:", err)
+	// Webサーバーを開始します
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -17,21 +17,17 @@ require (
 	cloud.google.com/go v0.67.0 // indirect
 	github.com/clbanning/x2j v0.0.0-20191024224557-825249438eec // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.1.1 // indirect
-	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/markbates/goth v1.71.1 // indirect
-	github.com/nsqio/go-nsq v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/codecs v0.0.0-20170403063245-04a5b1e1910d // indirect
 	github.com/stretchr/goweb v0.0.0-20140611034857-d7518e7a1cf8 // indirect
-	github.com/stretchr/signature v0.0.0-20160104132143-168b2a1e1b56 // indirect
 	github.com/stretchr/stew v0.0.0-20130812190256-80ef0842b48b // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/stretchr/tracer v0.0.0-20140124184152-66d3696bba97 // indirect
@@ -40,7 +36,6 @@ require (
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
-	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
