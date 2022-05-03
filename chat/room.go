ad2fe9beaf10a855afb796a39c195719c591eb70
ryuyauetake <ryuya@anymindgroup.com>
2022-05-03 22:11:43 +0900

    feat(room): chapter 1.3

--- chat/room.go
+++ chat/room.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです。
 	// ここで受け取ったメッセージを全て転送する。
@@ -11,4 +18,71 @@ type room struct {
 	leave chan *client
 	// [map]clientsには在室している全てのクライアントをjoin,leaveによって保持。
 	clients map[*client]bool
+}
+
+// newRoomはすぐに利用できるチャットルームを生成して返します。
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			//参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			// goのチャネルを閉じている
+			close(client.send)
+		case msg := <-r.forward:
+			// すべてのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+				default:
+					// 送信に失敗(クリーンアップの処理)
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+// Websocketを使用するためにwebsocket.Upgrader型を使用
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// Websocketのコネクションを取得
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	// Websocketのコネクションを取得したらclientを生成してjoinチャネルに渡す
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	// defer文(関数の終了処理)でクライアント終了時に退室。(クリーンアップの処理)
+	defer func() { r.leave <- client }()
+
+	// goルーチンの別スレッドとして以下が実行
+	go client.write()
+	client.read()
 }
