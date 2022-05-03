2d7ab4143aeef873646a0346ce1049e5305df88e
ryuyauetake <ryuya@anymindgroup.com>
2022-05-03 22:40:24 +0900

    feat(main): chapter 1.3.1

--- chat/main.go
+++ chat/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	// text/templateよりhtml/templateの方がセキュア。
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -23,11 +24,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		// mustはerrorが起きた時にnon-nilを返す。ParseFilesでテンプレートをパース。
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	// nilはtsでいうany型。https://stackoverflow.com/questions/64347531/what-is-the-type-of-nil
-	t.templ.Execute(w, nil)
+	// http.Requestにあるポート番号を参照できるようにする
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	// フラグを解釈します(flagの値を取得できるようにする)
+	flag.Parse()
 	r := newRoom()
 	// ルート
 	http.Handle("/", &templateHandler{filename: "chat.html"})
@@ -35,7 +39,8 @@ func main() {
 	// チャットルームを開始します
 	go r.run()
 	// Webサーバーを開始します
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Webサーバーを開始します。ポート:", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
 }
