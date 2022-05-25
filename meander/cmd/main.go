dd2fd0beaaec616a1ca3087095ce01765742f6e5
ryuyauetake <ryuya@anymindgroup.com>
2022-05-26 00:21:35 +0900

    feat 7.3.5

--- meander/cmd/main.go
+++ meander/cmd/main.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"os"
 	"runtime"
+	"strconv"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
@@ -26,6 +28,18 @@ func main() {
 		respond(w, r, meander.Journeys)
 	})
 	http.ListenAndServe(":8080", http.DefaultServeMux)
+	http.HandleFunc("/recommendations", func(w http.ResponseWriter, r *http.Request) {
+		// meanderQueryオブジェクトを生成
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+		q.Lat, _ = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		q.Lng, _ = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		q.Radius, _ = strconv.Atoi(r.URL.Query().Get("radius"))
+		q.CostRangeStr = r.URL.Query().Get("cost")
+		places := q.Run()
+		respond(w, r, places)
+	})
 }
 
 func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
