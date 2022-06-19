f233365de60cea1d9ff9ccf1a6fb3bce5eabc164
ryuyauetake <ryuya@anymindgroup.com>
2022-06-19 18:29:41 +0900

    wip: golang refactor userId and avatar url

--- chat/auth.go
+++ chat/auth.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
 	"github.com/stretchr/objx"
 )
@@ -16,16 +19,23 @@ type ChatUser interface {
 type chatUser struct {
 	httpWriter  http.ResponseWriter
 	httpRequest *http.Request
-}
-
-func (u chatUser) AvatarURL() string {
-	//TODO implement me like UniqueID()
-	panic("implement me")
+	uniqueID    string
+	goth.User
 }
 
 type authHandler struct {
 	// ラップ対象のハンドラを保持
 	next http.Handler
+}
+
+func (u chatUser) AvatarURL() string {
+	// 外部サービスからの認証結果を判定
+	user, err := gothic.CompleteUserAuth(u.httpWriter, u.httpRequest)
+	if err != nil {
+		fmt.Fprintln(u.httpWriter, err)
+		return ""
+	}
+	return user.AvatarURL
 }
 
 func (u chatUser) UniqueID() string {
@@ -58,22 +68,20 @@ func MustAuth(handler http.Handler) http.Handler {
 }
 
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/html")
-	_, err := w.Write([]byte(`<p>認証が完了しました</p>`))
-	if err != nil {
-		panic(err)
-	}
 
-	return
 	chatUser := &chatUser{httpWriter: w, httpRequest: r}
-	// TODO どこでやる?: chatUser.uniqueID = fmt.Sprintf("%x", m.Sum(nil))
+	m := md5.New()
+	chatUser.uniqueID = fmt.Sprintf("%x", m.Sum(nil))
+	avatarURL, err := avatars.GetAvatarURL(chatUser)
+	if err != nil {
+		log.Fatalln("GetAvatarURLに失敗しました", "-", err)
+	}
 
 	// 外部サービスから取得した情報をアプリ用データとしてCookieにしこむ
 	authCookieValue := objx.New(map[string]interface{}{
-		"userId":     chatUser.UniqueID(),
+		"userId":     chatUser.uniqueID,
 		"name":       chatUser.UniqueID(),
-		"avatar_url": chatUser.AvatarURL(),
+		"avatar_url": avatarURL,
 	}).MustBase64()
 	http.SetCookie(w, &http.Cookie{
 		Name:  "auth",
